7f8ee8c335ab057f1adc60af2efc59b20c9d9994
Karleen Richards <karleenmsrichards@gmail.com>
2023-04-18 12:02:42 +0100

    mounted express static middleware to the /public path

--- myApp.js
+++ myApp.js
@@ -1,6 +1,9 @@
 let express = require("express");
 let app = express();
 
+const path = require("path");
+app.use("/public", express.static(path.join(__dirname, "public")));
+
 app.get("/", function (request, response) {
   absolutePath = __dirname + "/views/index.html";
   response.sendFile(absolutePath);
